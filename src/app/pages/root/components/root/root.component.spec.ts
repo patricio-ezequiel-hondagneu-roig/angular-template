aee63e2c2f4f5d358fd085468b7e7c2ff567f92e
Patricio Ezequiel Hondagneu Roig <5291103+patricio-ezequiel-hondagneu-roig@users.noreply.github.com>
2020-09-17 00:58:55 -0300

    Update Angular version
    Update the repo to account for the changes introduced in the latest Angular version, including the deprecation of the testing "async" function and "solution style" tsconfig files.

--- src/app/pages/root/components/root/root.component.spec.ts
+++ src/app/pages/root/components/root/root.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RootComponent } from './root.component';
 
@@ -7,7 +7,7 @@ describe( 'RootComponent', ( ) => {
 	let fixture: ComponentFixture<RootComponent>;
 	let component: RootComponent;
 
-	beforeEach( async( ( ) => {
+	beforeEach( waitForAsync( ( ) => {
 		TestBed
 			.configureTestingModule({
 				imports: [
